f03bbf760d50fb82b44a2047a5493a4f2e85a953
let <oof.exe.lol@gmail.com>
2024-04-06 11:24:24 -0300

    Initial Commit #2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<!-- n fassa igual :3 -->
+
+<script lang="ts">
+    
+</script>
+
+<div
+    class="w-full h-full flex flex-col items-center align-center content-center justify-center text-white"
+>
+    <h1 class="text-blue-500 text-5xl">レタス</h1>
+    <p class="text-xl font-pixelify">hi, im letiul</p>
+</div>
