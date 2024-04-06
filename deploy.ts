cf00e918c95e85bb75fbec6e90e7ef5dffbd960f
let <oof.exe.lol@gmail.com>
2024-04-06 13:41:33 -0300

    teste

--- deploy.ts
+++ deploy.ts
@@ -1,5 +1,41 @@
 import enquirer from 'enquirer'
-import { execSync } from 'child_process'
+import { exec } from 'child_process'
+import loading from 'loading-cli'
+import util from 'util'
+import {getRandomQuote} from 'randoquoter'
+import { fail } from '@sveltejs/kit'
+
+const prom_exec = util.promisify(exec)
+
+function Loader_({
+    color,
+    frames = ["[-]", "[/]", "[|]", "[\\]"],
+    interval = 75,
+    stream = process.stdout,
+    text = "placeholder"
+}: loading.Options)
+{
+    const load = loading({
+        color,
+        frames,
+        interval,
+        stream,
+        text
+    })
+    load.start()
+    return {
+        resolve: (text: string) => load.succeed(text),
+        fail: (text: string) => load.fail(text),
+    }
+}
+
+function handle_error(error: string, failCallback: (text: string) => any) {
+    if (error.length > 0)  {
+        failCallback(error)
+        process.exit(1)
+    }
+    return
+}
 
 enquirer.prompt([
     {
@@ -21,28 +57,51 @@ enquirer.prompt([
         build: boolean,
         deploy: boolean
     } = response_ as any
-    // maybe add pico colors to make terminal look color
 
     if (response.build) {
-        console.log("[-] building app")
-        // maybe add a loading indicator
-        execSync("npm run build")
-        console.log("[+] builded app")
+        const {fail,resolve} = Loader_({
+            text:"Building app",
+        })
+
+        const { stderr } = await prom_exec("npm run build")
+        
+        if (stderr.length > 0) {
+            fail("Failed building app: " + stderr)
+            process.exit(1)
+        }
+        resolve("Builded app")
     }
 
-    console.log('[+] adding files')
-    execSync("git add .")
-    execSync(`git commit -m ${response.commit_name}`)
-    execSync("git push -u origin main")
+    const {fail,resolve} = Loader_({
+        text:"Adding files to remote repository",
+    })
+
+    const e = exec("git add .")
+    await new Promise((a) => e.on("close",a))
+    if (e.exitCode != 0) {
+        handle_error(e.stderr?.read(), fail)
+    }
+    
+
+    const {stderr:stderr2} = await prom_exec(`git commit -m "${response.commit_name}"`)
+    handle_error(stderr2, fail)
+
+    const {stderr:stderr3} = await prom_exec(`git commit -m "${response.commit_name}"`)
+    handle_error(stderr3, fail)
+
+    resolve("Added files")
 
     if (response.deploy) {
-        console.log('[+] deploying')
-        execSync("npm run deploy")
+        const {fail,resolve} = Loader_({
+            text:"Deploying to pages",
+        })
+        const {stderr} = await prom_exec("npm run deploy")
+        handle_error(stderr, fail)
+        resolve("Deployed to gh-pages")
     }
-
-    console.log("[+] deploy successful!")
-    console.log("did you know that 90% of people that quit gambling addiction, stop before they hit it big?")
-
+    
+    let quote = getRandomQuote();
+    console.log(`\n"${quote.text}"\n- ${quote.author}`)
 })
 
-export {}
+export { }
---
+++ out/_app/immutable/entry/start.BJH1TT5c.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CtE4yPdY.js";export{t as start};
